6fc2b7266e0014c221777b320a5ab0253dc1cb17
leederacola <rmgforce@gmail.com>
2017-06-12 10:26:11 -0400

    Added context class, EFDbContest, created EFDbProductRepo class and bound it with IProductRepo using magic!

---
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+
+/*
+ * This is the repository class. It implements the IProductRepository interface and uses
+ * an instance of EFDbContext to retrieve data from the database using the Entity Framework. 
+ * You will see how I work with the Entity Framework (and how simple it is) as I add features to the repository.
+ * To use the new repository class, I need to edit the Ninject bindings and replace the mock repository with a binding for the real one.
+ * Edit the NinjectDependencyResolver.cs class file in the SportsStore.WebUI project so that the AddBindings method looks like Listing 7-15.
+ */
+namespace SportsStore.Domain.Concrete
+{
+
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,14 +5,15 @@ using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 /*
  *  In Chapter 6, I showed you how to use Ninject to create a custom dependency resolver that the MVC Framework
  *  will use to instantiate objects across the application. I am going to repeat that process, starting with 
  *  adding an Infrastructure folder to the SportsStore.WebUI project and adding a class file called 
  *  NinjectDependencyResolver.cs to it. You can see the contents of the new file in Listing 7-1.
-*/
-/*
+ *
+ * 
  * Making a Mock Repository
  *  I had to add a number of namespaces to the file for this addition, but the process I used to create the mock repository
  *  implementation uses the same Moq techniques I introduced in Chapter 6. I want Ninject to return the same mock object whenever
@@ -23,7 +24,16 @@ using SportsStore.Domain.Entities;
         ...
  *  Rather than create a new instance of the implementation object each time, Ninject will always satisfy requests for the
  *  IProductRepository interface with the same mock object.
-*/
+ *  
+ *  
+ *  
+ * This is the repository class. It implements the IProductRepository interface and uses
+ * an instance of EFDbContext to retrieve data from the database using the Entity Framework. 
+ * You will see how I work with the Entity Framework (and how simple it is) as I add features to the repository.
+ * To use the new repository class, I need to edit the Ninject bindings and replace the mock repository with a binding for the real one.
+ * Edit the NinjectDependencyResolver.cs class file in the SportsStore.WebUI project so that the AddBindings method looks like Listing 7-15.
+ */
+
 namespace SportsStore.WebUI.Infrastructure
 {
     public class NinjectDependencyResolver : IDependencyResolver
@@ -49,14 +59,7 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
-
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
