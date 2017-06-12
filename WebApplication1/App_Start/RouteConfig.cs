60acdd399ea2a22272cf313cb79e2aae41548bd2
leederacola <rmgforce@gmail.com>
2017-06-12 11:20:09 -0400

    Finally Runs after using localDB. Messed up web.congif files. PAY ATTENTION TO BOTH OF THEM!

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -1,12 +1,11 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SportsStore.WebUI
+namespace WebApplication1
 {
     public class RouteConfig
     {
@@ -17,12 +16,7 @@ namespace SportsStore.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new
-                {
-                    controller = "Product",
-                    action = "List",
-                    id = UrlParameter.Optional
-                }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
