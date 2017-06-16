9dbadb009088f2aa8c453c8908307e504e6c0b14
leederacola <rmgforce@gmail.com>
2017-06-16 10:57:04 -0400

    edited some url routing using Url.Action method, need to read up

--- SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -5,23 +5,56 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SportsStore.WebUI {
-    public class RouteConfig {
-        public static void RegisterRoutes(RouteCollection routes) {
+/*
+ * URL
+Leads To
+/
+Lists the first page of products from all categories
+/Page2
+Lists the specified page (in this case, page 2), showing items from all categories
+/Soccer
+Shows the first page of items from a specific category (in this case, the Soccer category)
+/Soccer/Page2
+Shows the specified page (in this case, page 2) of items from the specified category (in this case, Soccer)
+ */
+
+namespace SportsStore.WebUI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { Controller = "Product", action = "List" }
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", 
-                    id = UrlParameter.Optional }
+            routes.MapRoute(null,
+                "Page{page}",
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" }
             );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
