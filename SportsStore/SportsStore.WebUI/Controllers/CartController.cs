1753ed9b27a7f22cef23380d618e4b017519a136
leederacola <rmgforce@gmail.com>
2017-06-19 12:15:35 -0400

    Finished Chapter 8, copied source code to remove possibile errors moving foward. (Previsouly added Ch 9 source) Fixing this error.

--- SportsStore/SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -8,65 +8,46 @@ namespace SportsStore.WebUI.Controllers {
 
     public class CartController : Controller {
         private IProductRepository repository;
-        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductRepository repo, IOrderProcessor proc) {
+        public CartController(IProductRepository repo) {
             repository = repo;
-            orderProcessor = proc;
         }
 
-
-        public ViewResult Index(Cart cart, string returnUrl) {
+        public ViewResult Index(string returnUrl) {
             return View(new CartIndexViewModel {
-                ReturnUrl = returnUrl,
-                Cart = cart
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
             });
         }
 
-        public RedirectToRouteResult AddToCart(Cart cart, int productId,
-                string returnUrl) {
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl) {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null) {
-                cart.AddItem(product, 1);
+                GetCart().AddItem(product, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId,
-                string returnUrl) {
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl) {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null) {
-                cart.RemoveLine(product);
+                GetCart().RemoveLine(product);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public PartialViewResult Summary(Cart cart) {
-            return PartialView(cart);
-        }
+        private Cart GetCart() {
 
-        public ViewResult Checkout() {
-            return View(new ShippingDetails());
-        }
-
-        [HttpPost]
-        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails) {
-            if (cart.Lines.Count() == 0) {
-                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null) {
+                cart = new Cart();
+                Session["Cart"] = cart;
             }
-
-            if (ModelState.IsValid) {
-                orderProcessor.ProcessOrder(cart, shippingDetails);
-                cart.Clear();
-                return View("Completed");
-            } else {
-                return View(shippingDetails);
-            }
+            return cart;
         }
-
     }
 }
