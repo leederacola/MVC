bf26f54c738ca6651e9def4ad1a86720a5df48f9
leederacola <rmgforce@gmail.com>
2017-06-19 14:42:37 -0400

    9-18 Order processor stuff I dont quite comprehend

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
@@ -8,26 +8,41 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
-namespace SportsStore.WebUI.Infrastructure {
+namespace SportsStore.WebUI.Infrastructure
+{
 
-    public class NinjectDependencyResolver : IDependencyResolver {
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
         private IKernel kernel;
 
-        public NinjectDependencyResolver(IKernel kernelParam) {
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
             kernel = kernelParam;
             AddBindings();
         }
 
-        public object GetService(Type serviceType) {
+        public object GetService(Type serviceType)
+        {
             return kernel.TryGet(serviceType);
         }
 
-        public IEnumerable<object> GetServices(Type serviceType) {
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
             return kernel.GetAll(serviceType);
         }
 
-        private void AddBindings() {
+        private void AddBindings()
+        {
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                    .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
     }
 }
